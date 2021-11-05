76195ae70247f5ccf3fa868baac403e2c345aa58
RedRiderX <contactus@redriderx.com>
2021-11-05 17:30:51 -0400

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  let name = "world";
+</script>
+
+<h1>Hello {name}!</h1>
+
+<div class="header">
+  <h1>
+    <img src="logo.svg" alt="Spell: The RPG" class="logo" /> Tile Generator
+  </h1>
+</div>
+<div class="options">Enter tile number . Roll button</div>
+<div class="tiles">X tiles</div>
+<div class="input">Entry area. Spell button</div>
+<div class="spell-list">Found spells</div>
+
+<style>
+  .logo {
+    display: inline-block;
+    width: 12rem;
+    vertical-align: middle;
+  }
+</style>
